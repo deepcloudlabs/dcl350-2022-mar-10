bada383a3b28d33af4c43d6814d29a2a50da8fba
deepcloudlabs <deepcloudlabs@gmail.com>
2022-03-15 10:12:24 +0300

    add eureka server

---
+++ registry-server/src/test/java/com/example/reigstry/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reigstry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
